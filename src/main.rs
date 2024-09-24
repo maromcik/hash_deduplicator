71283175c4a3bd2a8815ed31b09b20ce0b652a28
Roman Alexander Mariancik <mariancik@ics.muni.cz>
2024-09-24 12:40:27 +0200

    first attempt

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ use crate::hash::process;
 mod search;
 mod hash;
 use std::env;
+use std::fs::File;
+use std::io::{BufWriter, Write};
 use crate::search::list_files;
 
-fn main() -> io::Result<()>{
+fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let paths = args.into_iter().skip(1).collect::<Vec<String>>();
     println!("Looking in paths:");
@@ -15,17 +17,20 @@ fn main() -> io::Result<()>{
     let files = list_files(paths)?;
     let duplicates = process(files)?;
 
+    let deleted_files = File::create("deleted_files.txt")?;
+    let mut log = BufWriter::new(deleted_files);
+    let mut sum = 0;
     for (k, v) in duplicates.iter() {
         println!("digest: {:?}, path: {:?}", k, v);
-        let _ = v
-            .into_iter()
-            .skip(1)
-            .map(|f| delete_file(f))
-            .collect::<Vec<io::Result<()>>>();
+
+        for f in v.iter().skip(1) {
+            write!(log, "{}\n", f)?;
+            delete_file(f)?;
+            sum += 1;
+        }
     }
-
     println!("Duplicates count: {}", duplicates.len());
-
+    println!("Deleted files: {sum}");
     Ok(())
 }
 
